0ced58795c2f55767027144abfc483734d7da9c6
craigfay <craigfay1@gmail.com>
2021-12-31 10:22:00 -0600

    restructure files

---
+++ static/load_wasm.js
@@ -0,0 +1,14 @@
+
+// Loading and executing webassembly modules
+
+const go = new Go();
+
+WebAssembly.instantiateStreaming(
+  fetch("dist/alert.wasm", { cache: 'no-cache' }),
+  go.importObject,
+)
+  .then(async (result) => {
+    mod = result.module;
+    inst = result.instance;
+    await go.run(inst);
+  });
