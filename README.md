ae3f04e680dcfe852112cc44bd48306cd4cf90cd
craigfay <craigfay1@gmail.com>
2021-12-31 10:01:59 -0600

    serve.go

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 
 # Usage
 * build: `GOARCH=wasm GOOS=js go build -o dist/alert.wasm alert.go`
+* start: `go run serve.go`
---
+++ serve.go
@@ -0,0 +1,26 @@
+/*
+Usage: `go run serve.go`
+	-p="3000": port to serve on
+	-d=".":    the directory of static files to host
+Navigating to http://localhost:<port>will display the index.html or directory
+listing file.
+*/
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := flag.String("p", "3000", "port to serve on")
+	directory := flag.String("d", ".", "the directory of static file to host")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(*directory)))
+
+	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
