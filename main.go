31510d021f57bb197a645d56e17aed86f4fc23e5
craigfay <craigfay1@gmail.com>
2021-12-31 14:44:05 -0600

    helper fns

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ package main
 import (
     "fmt"
     "syscall/js"
-    . "github.com/siongui/godom/wasm"
 )
 
 var signal = make(chan int)
@@ -28,27 +27,43 @@ func setAttr(el js.Value, key string, val string) {
 }
 
 type EventHandler = func(this js.Value, args []js.Value) interface {}
+
+
 func event(el js.Value, eventName string, fn EventHandler) {
     el.Call("addEventListener", eventName, js.FuncOf(fn))
+}
+
+func query(selector string) js.Value {
+    return js.Global().Get("document").Call("querySelector", selector)
+}
+
+func queryAll(selector string) js.Value {
+    return js.Global().Get("document").Call("querySelectorAll", selector)
+}
+
+func append(parent js.Value, child js.Value) {
+    parent.Call("appendChild", child)
 }
 
 func main() {
     count := 0
 
-    // div := Document.CreateElement("div")
     div := element("div")
-    setAttr(div, "textContent", fmt.Sprintf("I am clicked %d time", count))
+
+    text := fmt.Sprintf("I am clicked %d time", count)
+    setAttr(div, "textContent", text)
 
     onClick := func(this js.Value, args []js.Value) interface {} {
         count++
-        setAttr(div, "textContent", fmt.Sprintf("I am clicked %d time", count))
+        text := fmt.Sprintf("I am clicked %d time", count)
+        setAttr(div, "textContent", text)
         return js.Value {}
     }
 
     event(div, "click", onClick)
 
-    root := Document.GetElementById("root")
-    root.Call("appendChild", div)
+    root := query("#root")
+    append(root, div)
 
     keepAlive()
 }
